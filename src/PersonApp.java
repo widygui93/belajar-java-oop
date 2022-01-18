32ded1fd001ad88a6257aad234c9fe2876c9e830
Widy Guilias <guilias.widy@gmail.com>
2022-01-18 21:27:38 +0700

    belajar sampai this keyword, besok lanjut ke inheritance

---
+++ src/PersonApp.java
@@ -0,0 +1,22 @@
+public class PersonApp {
+    public static void main(String[] args) {
+        var person1 = new Person("Widy", "Medan");
+
+//        person1.country = "Malaysia"; // tidak bisa assign value yg final
+
+        System.out.println(person1);
+        System.out.println(person1.name);
+        System.out.println(person1.address);
+        System.out.println(person1.country);
+        person1.sayHello("Budi");
+
+
+        Person person2 = new Person("Budi");
+
+
+        Person person3;
+        person3 = new Person();
+        person3.name = "Susan";
+        person3.sayHello("KOKO");
+    }
+}
